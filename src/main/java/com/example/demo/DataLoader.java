37a4ba4eba1a82af4a25f6512805722428e7e53b
KathyOlsen <katherinekolsen@verizon.net>
2019-10-18 14:40:27 -0400

    got dataloader being conditional to work

--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -20,7 +20,7 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception{
-//        if(roleRepository.findAll() == null) {
+        if(roleRepository.findByRole("'ADMIN'") == null) {
             roleRepository.save(new Role("USER"));
             roleRepository.save(new Role("ADMIN"));
 
@@ -29,14 +29,14 @@ public class DataLoader implements CommandLineRunner {
 
             User user = new User("jim@jim.com", "password",
                     "Jim", "Jimmerson", true, "jim");
-            user.setRoles(Arrays.asList(userRole,adminRole));
+            user.setRoles(Arrays.asList(userRole));
             userRepository.save(user);
 
             user = new User("admin@admin.com", "password",
                     "Admin", "User", true, "admin");
             user.setRoles(Arrays.asList(adminRole));
             userRepository.save(user);
-//        }
+        }
 
     }
 }
